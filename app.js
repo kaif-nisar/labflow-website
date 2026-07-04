09abea8b511066b805affe6add3921d4fc525552
kaif-nisar <kaifquest786@gmail.com>
2026-07-04 22:03:10 +0530

    f

--- app.js
+++ app.js
@@ -395,8 +395,8 @@ app.use((req, res, next) => {
 // 📋 STANDARD MIDDLEWARES
 // ========================
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
